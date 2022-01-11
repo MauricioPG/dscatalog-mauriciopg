76eb12b9ee1e4f8132ee2249f348e8f85f45e27a
MauricioPG <mauricio_guidini@msn.com>
2022-01-11 08:33:26 -0300

    Login Card Style

--- frontweb/src/pages/Admin/Auth/index.tsx
+++ frontweb/src/pages/Admin/Auth/index.tsx
@@ -1,10 +1,12 @@
 import { ReactComponent as AuthImage } from "assets/images/auth-image.svg";
 import { Route, Switch } from "react-router-dom";
+import Login from "./Login";
+import './styles.css';
 
 const Auth = () => {
     return (
         <div className="auth-container">
-            <div className="auth-nbanner-container">
+            <div className="auth-banner-container">
                 <h1>Divulgue seus produtos no DS Catalog</h1>
                 <p>Faça parte do nosso catálogo de divulgação 
                     e aumente a venda dos seus produtos.</p>
@@ -13,7 +15,7 @@ const Auth = () => {
             <div className="auth-form-container">
                 <Switch>
                     <Route path="/admin/auth/login">
-                        <h1>Card LOGIN</h1>
+                        <Login />
                     </Route>
                     <Route path="/admin/auth/signup">
                         <h1>Card SIGNUP</h1>
