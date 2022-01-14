712312ba367b2575a84ad7ac2b7b809123f9730b
MauricioPG <mauricio_guidini@msn.com>
2022-01-14 11:49:57 -0300

    Login/Logout logic

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -2,12 +2,10 @@ import './styles.css';
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from '../../components/ButtonIcon';
 import { Link } from 'react-router-dom';
-import { isAuthenticated } from 'util/requests';
 
 const Home = () => {
   return (
     <div className="home-container">
-      <h1> { isAuthenticated() ? "SIM" : "NÃO" } </h1>
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
